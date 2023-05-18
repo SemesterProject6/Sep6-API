f127cad385ec8b5641be42c55ea7c940be303e2c
StapanulLumii <82084902+StapanulLumii@users.noreply.github.com>
2023-05-18 13:36:44 +0200

    Movie controller, service and models

---
+++ Sep6-API/Controllers/MovieController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sep6_API.Models;
+using SEP6_API.Data.Movies;
+
+namespace Sep6_API.Controllers
+{
+    public class MovieController : Controller
+    {
+        IMovieService movieService;
+
+        public MovieController(IMovieService movieService)
+        {
+            this.movieService = movieService;
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Movie))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Movie>> GetMovieByID(int id)
+        {
+            try
+            {
+                Movie movie = await movieService.GetMovieByID(id);
+                return Ok(movie);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+        [HttpGet("ByRating")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ListOfMovies))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ListOfMovies>> GetMoviesByRating(int page)
+        {
+            try
+            {
+                ListOfMovies movies = await movieService.GetMoviesByRating(page);
+                return Ok(movies);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+        [HttpGet("ByTitle")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ListOfMovies))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ListOfMovies>> GetMoviesByTitle(int page)
+        {
+            try
+            {
+                ListOfMovies movies = await movieService.GetMoviesByTitle(page);
+                return Ok(movies);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+        [HttpGet("search/")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ListOfMovies))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ListOfMovies>> SearchForMovies([FromQuery] int page, [FromQuery] string query)
+        {
+            try
+            {
+                ListOfMovies movies = await movieService.GetMoviesBySearch(page, query);
+                return Ok(movies);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
---
+++ Sep6-API/Data/Movies/MovieService.cs
@@ -0,0 +1,61 @@
+﻿using Sep6_API.Models;
+using SEP6_API.Data.Movies;
+using System.Text.Json;
+
+namespace SEP6_Api.Data.Movies
+{
+    public class MovieService : IMovieService
+    {
+
+        string url = "https://api.themoviedb.org/3/movie/";
+        string newUrl = "https://api.themoviedb.org/3/discover/movie";
+        HttpClient client;
+        private readonly IConfiguration configuration;
+        private string apiKey;
+
+        public MovieService(IConfiguration iConfig)
+        {
+            client = new HttpClient();
+            configuration = iConfig;
+            apiKey = configuration["APIKeys:ApiKey"];
+        }
+
+        public async Task<Movie> GetMovieByID(int id)
+        {
+            string message = await client.GetStringAsync(url + id + apiKey);
+            Movie movie = JsonSerializer.Deserialize<Movie>(message);
+            return movie;
+        }
+
+        public async Task<ListOfMovies> GetMoviesBySearch(int page, string query)
+        {
+            string newUrl = url.Remove(url.IndexOf('3') + 1); 
+            Console.WriteLine(newUrl);
+            var moviesUrl = newUrl + "/search/movie" + apiKey + "&query=" + query + "&page=" + page;
+            string message = await client.GetStringAsync(moviesUrl);
+            ListOfMovies results = JsonSerializer.Deserialize<ListOfMovies>(message);
+            return results;
+        }
+
+        public async Task<ListOfMovies> GetMoviesByRating(int page)
+        {
+            var moviesUrl = apiKey + "&language=en-US&sort_by=vote_average.asc&vote_count.gte=215&vote_average.gte=3&page=";
+            if (page != 0)
+                moviesUrl += page;
+            string message = await client.GetStringAsync(newUrl + moviesUrl);
+            ListOfMovies results = JsonSerializer.Deserialize<ListOfMovies>(message);
+            return results;
+        }
+      
+
+        public async Task<ListOfMovies> GetMoviesByTitle(int page)
+        {
+            var moviesUrl = apiKey + "&language=en-US&sort_by=original_title.asc&vote_count.gte=215&vote_average.gte=3&page=";
+            if (page != 0)
+                moviesUrl += page;
+            string message = await client.GetStringAsync(newUrl + moviesUrl);
+            ListOfMovies results = JsonSerializer.Deserialize<ListOfMovies>(message);
+            return results;
+        }
+    }
+}
