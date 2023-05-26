3f9884df5762c246a2519f77e0e7c87f2e1af32f
Valeriu1 <74401589+Valeriu1@users.noreply.github.com>
2023-05-26 12:19:17 +0200

    adding user object

---
+++ Sep6-API/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Sep6_API.Models
+{
+    public class User
+    {
+        [JsonPropertyName("UserID")]
+        public int? UserID { get; set; }
+        [JsonPropertyName("FirstName")]
+        public string? FirstName { get; set; }
+        [JsonPropertyName("LastName")]
+        public string? LastName { get; set; }
+        [JsonPropertyName("Birthday")]
+        public DateTime? Birthday { get; set; }
+        [JsonPropertyName("Age")]
+        public int? Age { get; set; }
+        [JsonPropertyName("Email")]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [JsonPropertyName("Password")]
+        public string? Password { get; set; }
+
+    }
+}
